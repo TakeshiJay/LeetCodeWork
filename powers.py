5a1837aec9b98aafdbf57f46704fafabb3a48034
Jacob Sunia <70661851+TakeshiJay@users.noreply.github.com>
2020-12-10 15:28:33 -0800

    Create powers.py

---
+++ powers.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        takes the power of the given input x as the base and n as the power
+        """
+        return(x**n)
